6f187523315bebd2339da421a40cb32200e78f2c
Santirbe98 <santirbe@gmail.com>
2023-03-27 10:28:01 -0300

    refactor(textChanged)

---
+++ .next/static/webpack/pages/index.29b0ebb6aab9a8a6.hot-update.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("pages/index",{
+
+/***/ "./src/data/sections/about-us3.json":
+/*!******************************************!*\
+  !*** ./src/data/sections/about-us3.json ***!
+  \******************************************/
+/***/ (function(module) {
+
+module.exports = JSON.parse('{"id":1,"image1":"/img/EventAbout1.png","image2":"/img/EventAbout2.png","exp":{"nmb":12,"name":"Años de experiencia"},"title":{"first":"Quienes somos","second":""},"content":{"first":"Somos un grupo interdisciplinario de profesionales que buscan crear experiencias innovadoras, a partir de la confianza y el compromiso con nuestros clientes. Usamos el más alto standard de calidad para la planificación y el desarrollo de eventos corporativos, académicos y comerciales.","second":"Gestionar eventos con diseño y comercialización de alta convocatoria, usando estrategia corporativa y organización integral basada en la investigación de proyectos para mercados que promuevan la sustentabilidad."}}');
+
+/***/ })
+
+});
---
+++ .next/static/webpack/pages/index.8a41a71d6a203219.hot-update.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("pages/index",{
+
+/***/ "./src/data/sections/works.json":
+/*!**************************************!*\
+  !*** ./src/data/sections/works.json ***!
+  \**************************************/
+/***/ (function(module) {
+
+module.exports = JSON.parse('[{"id":1,"image":"/img/portfolio/metro/01.jpg","slug":"/project-details2/project-details2-dark"},{"id":2,"image":"/img/portfolio/metro/02.jpg","slug":"/project-details2/project-details2-dark"},{"id":3,"image":"/img/portfolio/metro/03.jpg","slug":"/project-details2/project-details2-dark"},{"id":4,"image":"/img/portfolio/metro/04.jpg","slug":"/project-details2/project-details2-dark"},{"id":5,"image":"/img/portfolio/metro/05.jpg","slug":"/project-details2/project-details2-dark"}]');
+
+/***/ })
+
+});
